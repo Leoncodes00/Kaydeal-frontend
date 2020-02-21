ee5e24fad2c0a2859daa5c833ac1f28529f612c3
Leoncodes00 <chachaiyang3@gmail.com>
2020-02-21 12:13:02 -0800

    Updates

---
+++ precache-manifest.b95082680eace263a3dc9a707671feb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5429bdb6af225cb4f0608920c457c06e",
+    "url": "/Kaydeal-frontend/index.html"
+  },
+  {
+    "revision": "0a551264a875f8d6daa6",
+    "url": "/Kaydeal-frontend/static/css/main.f3e66055.chunk.css"
+  },
+  {
+    "revision": "2a0c86dd911c7e2d7815",
+    "url": "/Kaydeal-frontend/static/js/2.e868de5c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Kaydeal-frontend/static/js/2.e868de5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a551264a875f8d6daa6",
+    "url": "/Kaydeal-frontend/static/js/main.42374e04.chunk.js"
+  },
+  {
+    "revision": "79f6eb6f3b73e0d52a16",
+    "url": "/Kaydeal-frontend/static/js/runtime-main.eb227faa.js"
+  },
+  {
+    "revision": "69cfda92cd621b677e86189d43bcd09a",
+    "url": "/Kaydeal-frontend/static/media/classic-blue-coupe-die-cast-model-1037995.69cfda92.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Kaydeal-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
